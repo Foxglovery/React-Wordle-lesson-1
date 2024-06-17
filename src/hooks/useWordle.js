b6b1035afb8630fd0440a8e5f9c2682c7dc4bde7
Foxglovery <foxglovery@gmail.com>
2024-06-17 10:40:04 -0500

    adding guess logic

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -5,13 +5,15 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, SetCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
+  const [history, setHistory] = useState(["ninja", "hello"]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
 
   //format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("formatting the guess -", currentGuess);
+  };
   // add a new guess to the guesses state
   //update the isCorrect state if the guess is correct
   //add one to the turn state
@@ -20,6 +22,24 @@ const useWordle = (solution) => {
   // handle keyup event and track current guess
   // if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("guesses exhausted");
+        return;
+      }
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("you've been here before");
+        return;
+      }
+      // the word must be 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log("5 is the magic number");
+        return;
+      }
+      formatGuess();
+    }
     if (key === "Backspace") {
       SetCurrentGuess((prev) => {
         return prev.slice(0, -1);
